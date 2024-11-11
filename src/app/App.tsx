fd59bc2fdf8a15b2ae7aacadef098c1d2712085a
zlw00ng0419 <anggodji2422@snu.ac.kr>
2024-11-11 18:38:08 +0900

    Sub9 (#17)
    Co-authored-by: ihnsy <snu1127@snu.ac.kr>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,6 +14,7 @@ import image3 from '../../assets/google.png';
 import image4 from '../../assets/kakao.png';
 import logo from '../../assets/logo.svg';
 import styles from './App.module.css';
+import CheckToken from './CheckToken';
 import Login from './Login';
 import MyPage from './MyPage';
 import TimeTable from './TimeTable';
@@ -23,10 +24,11 @@ export const App = () => {
     <Router>
       <div className={styles.wrapper}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<CheckToken />} />
           <Route path="/login" element={<Login />} />
           <Route path="/timetable" element={<TimeTable />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
