0f32f68a37e2785ba1e4d111a3d73cc38b19fd40
ihnsy <snu1127@snu.ac.kr>
2024-11-11 21:05:15 +0900

    css 수정

--- src/app/TimeTable.tsx
+++ src/app/TimeTable.tsx
@@ -70,13 +70,13 @@ const TimeTable: React.FC = () => {
   console.debug(LectureList);
 
   return (
-    <div className="flex flex-col h-[100dvh] w-[100dvh]">
+    <div className="flex flex-col h-[100dvh]">
       <div className="flex h-[64px] py-[8px] px-[16px] border-b-[1px] border-b-[#C4C4C4] items-center gap-[10px]">
         <img src={list} className="w-[18px] h-[18px] mr-[5px]" />
         <h1 className="w-[25px] h-[24px] text-base content-center">a안</h1>
         <p className="w-[45px] h-[16px] text-xs text-[#C4C4C4]">(18학점)</p>
       </div>
-      <div className="flex h-[800px] flex-col">
+      <div className="flex flex-1 flex-col">
         <div className="grid w-full h-[30px] grid-cols-[5%_19%_19%_19%_19%_19%] divide-x divide-[#C4C4C4] border-y border-y-[#C4C4C4]">
           <div></div>
           {/* 빈자리 */}
