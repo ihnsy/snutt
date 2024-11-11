07c8e048315be82deb5daac5ff138ad5bcd0d889
zlw00ng0419 <anggodji2422@snu.ac.kr>
2024-11-11 19:37:39 +0900

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,6 @@ import image3 from '../../assets/google.png';
 import image4 from '../../assets/kakao.png';
 import logo from '../../assets/logo.svg';
 import styles from './App.module.css';
-import CheckToken from './CheckToken';
 import Login from './Login';
 import MyPage from './MyPage';
 import TimeTable from './TimeTable';
