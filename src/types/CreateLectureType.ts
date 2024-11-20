a817abc8cb1a8efd3745f0b77b8a4b46beeb7b51
ihnsy <snu1127@snu.ac.kr>
2024-11-20 20:22:58 +0900

    강의 생성 버그 픽스

--- src/types/CreateLectureType.ts
+++ src/types/CreateLectureType.ts
@@ -3,15 +3,8 @@ type ClassTime = {
   place: string; // 강의 장소
   startMinute: number; // 강의 시작 분
   endMinute: number; // 강의 종료 분
-  start_time: string; // 강의 시작 시간 (HH:mm 형식)
-  end_time: string; // 강의 종료 시간 (HH:mm 형식)
   len: number; // 강의 길이 (분 단위)
   start: number; // 강의 시작 시각 (분 단위)
-};
-
-type LectureColor = {
-  bg: string; // 배경색
-  fg: string; // 글자색
 };
 
 export type Lecture = {
@@ -20,7 +13,6 @@ export type Lecture = {
   credit: number; // 학점
   class_time_json: ClassTime[]; // 강의 시간 정보 배열
   remark: string; // 비고
-  color: LectureColor[]; // 색상 정보
   colorIndex: number; // 색상 인덱스
   is_forced: boolean; // 강제 여부
 };
